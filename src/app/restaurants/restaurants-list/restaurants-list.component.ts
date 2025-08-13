febd2b3f804db812f7e16d30d300f1a6b27c92e8
vedkulk <kulkarnived3@gmail.com>
2025-08-13 22:26:59 +0530

    restaurant module and service

---
+++ src/app/restaurants/restaurants-list/restaurants-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurants-list',
+  templateUrl: './restaurants-list.component.html',
+  styleUrls: ['./restaurants-list.component.css']
+})
+export class RestaurantsListComponent {
+
+}
---
+++ src/app/restaurants/restaurants.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RestaurantsListComponent } from './restaurants-list/restaurants-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    RestaurantsListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class RestaurantsModule { }
