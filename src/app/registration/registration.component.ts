fe5a1270a2cf5a9a91d94cfddff5a1dec031dbe3
vedkulk <kulkarnived3@gmail.com>
2025-08-18 21:00:42 +0530

    registration box

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-registration',
-  imports: [],
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
-
+  
+  onSubmit(form: any) {
+    console.log('Form Data:', form.value);
+  }  
 }
