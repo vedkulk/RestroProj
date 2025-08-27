1fdc1b58fdfc7472b3f4c655ead09e6408bac548
vedkulk <kulkarnived3@gmail.com>
2025-08-27 23:05:17 +0530

    bookings

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { BookingRequest } from './models/booking-request';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { BookingService } from './booking.service';
-
 
 @Component({
   selector: 'app-booking',
@@ -10,27 +9,29 @@ import { BookingService } from './booking.service';
   styleUrls: ['./booking.component.css']
 })
 export class BookingComponent {
-  bookingRequest !: BookingRequest;
+  bookingRequest!: BookingRequest;
+  RestaurantName = "Hello";
 
   bookingDetails = this.formBuilder.group({
-    email: new FormControl<string>('', [Validators.required, Validators.email]),
-    restaurant: new FormControl<string>('',Validators.required),
-    bookingDate: new FormControl<string>('', Validators.required),
-    bookingTime: new FormControl<string>('', Validators.maxLength(4)),
-    headCount: new FormControl<number>(1)
+    email: ['', [Validators.required, Validators.email]],
+    restaurant: [{ value: this.RestaurantName, disabled: true }],   
+    bookingDate: ['', Validators.required],
+    bookingTime: ['', Validators.required],
+    headCount: [1, [Validators.required, Validators.min(1)]]
   });
 
-  constructor(private formBuilder : FormBuilder, private bookingService : BookingService){}
+  constructor(private formBuilder: FormBuilder, private bookingService: BookingService) {}
 
-  captureBookingDetails() : void {
-    console.log(this.bookingDetails);
-    if(this.bookingDetails.valid){  
+  captureBookingDetails(): void {
+    console.log("Button clicked!", this.bookingDetails.value);
+
+    if (this.bookingDetails.valid) {
       this.bookingRequest = new BookingRequest();
-      this.bookingRequest.email = this.bookingDetails.value.email == null ? '' : this.bookingDetails.value.email; 
-      this.bookingRequest.bookingDate = this.bookingDetails.value.bookingDate == null ? '' : this.bookingDetails.value.bookingDate;
-      this.bookingRequest.restaurant = this.bookingDetails.value.restaurant == null ? '' : this.bookingDetails.value.restaurant;
-      this.bookingRequest.bookingTime = this.bookingDetails.value['bookingTime'] == null ? '' : this.bookingDetails.value["bookingTime"];
-      //this.bookingRequest.headCount = this.bookingDetails.value.headCount;
+      this.bookingRequest.email = this.bookingDetails.get('email')?.value || '';
+      this.bookingRequest.bookingDate = this.bookingDetails.get('bookingDate')?.value || '';
+      this.bookingRequest.bookingTime = this.bookingDetails.get('bookingTime')?.value || '';
+      this.bookingRequest.headCount = this.bookingDetails.get('headCount')?.value || 1;
+
       this.bookingService.createBooking(this.bookingRequest);
     }
   }
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-registration',
-  standalone: true, 
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
